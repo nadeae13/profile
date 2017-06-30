81ff0cacbaede29380ee83010b312250dbf4afe4
nadia <nadeardiiz13@gmail.com>
2017-06-30 16:35:55 +0700

    creact project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ProfileContent from './ProfileContent'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <ProfileContent/>
+      </div>
+
+    );
+  }
+}
+
+export default App;
